66ff09a93e661714c9f462b6fb06563d50e7c7e9
Vaishnavi <vaishnavi.kommareddy@valuelabs.net>
2014-06-23 18:02:42 +0530

    commit

---
+++ sample/sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by Valuelabs on 31/07/13.
+//  Copyright (c) 2013 Valuelabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sample/sampleTests/sampleTests.m
@@ -0,0 +1,36 @@
+//
+//  sampleTests.m
+//  sampleTests
+//
+//  Created by Valuelabs on 31/07/13.
+//  Copyright (c) 2013 Valuelabs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sampleTests : XCTestCase
+
+@end
+
+@implementation sampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

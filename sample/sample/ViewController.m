250bf78be0d7da5cbd5d363d180a6dde86c1bdd7
Vaishnavi <vaishnavi.kommareddy@valuelabs.net>
2014-06-25 16:58:46 +0530

    change

--- sample/sample/ViewController.m
+++ sample/sample/ViewController.m
@@ -20,7 +20,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"Sample Application");
-    NSLog(@"merging");
+    NSLog(@"merging---");
 }
 
 - (void)didReceiveMemoryWarning

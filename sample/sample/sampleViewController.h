c8a36dcb322ecbfaa5a6cb0cea05792bea866577
Vaishnavi <vaishnavi.kommareddy@valuelabs.net>
2014-06-25 17:10:39 +0530

    added new files

---
+++ sample/sample/sampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  sampleViewController.h
+//  sample
+//
+//  Created by Valuelabs on 25/06/14.
+//  Copyright (c) 2014 Valuelabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sampleViewController : UIViewController
+
+@end
---
+++ sample/sample/sampleViewController.m
@@ -0,0 +1,38 @@
+//
+//  sampleViewController.m
+//  sample
+//
+//  Created by Valuelabs on 25/06/14.
+//  Copyright (c) 2014 Valuelabs. All rights reserved.
+//
+
+#import "sampleViewController.h"
+
+@interface sampleViewController ()
+
+@end
+
+@implementation sampleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
